c6ec18eab802661d3b16042f6b0b95425a8879b0
Adetola Alexander Agunbiade <aa.adetola@outlook.com>
2024-10-29 16:24:00 -0400

    test components locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import React, { useState } from "react";
 import { Button } from "react-hive";
+import { TextInput } from "../lib/TextInput/TextInput";
 
 function App() {
+  const [value, setValue] = useState("");
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(event.target.value);
+  console.log(`value is: ${value}`);
   return (
-    <>
+    <div className="tw-col">
       <Button>Test</Button>
-    </>
+      <TextInput
+        handleUserInput={handleInput}
+        value={value}
+        size={"lg"}
+        // styles="w-full"
+        style={{ width: "100%" }}
+      />
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     // reference the library only
     "./lib/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
