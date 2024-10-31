0da77c98332837ada26ead37ccca0cc1ec0effcb
Adetola Alexander Agunbiade <aa.adetola@outlook.com>
2024-10-31 12:52:44 -0400

    Update compoenent for viewing components in dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
 import { Button } from "react-hive";
 import { TextInput } from "../lib/TextInput/TextInput";
+import { Login } from "../lib/Login/Login";
 
 function App() {
   const [value, setValue] = useState("");
-  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setValue(event.target.value);
+  const handleInput = (value: string) => setValue(value);
   console.log(`value is: ${value}`);
   return (
     <div className="tw-col">
       <Button>Test</Button>
       <TextInput
+        type="text"
         handleUserInput={handleInput}
         value={value}
         size={"lg"}
         // styles="w-full"
         style={{ width: "100%" }}
       />
+      <Login />
     </div>
   );
 }
